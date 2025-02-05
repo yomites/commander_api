ae1309580771064a8db1be8feb89f59b09557968
Oriyomi Oladele <marshall_400@yahoo.com>
2025-02-05 12:28:02 +0200

    Addition of txt file for the real robot waypoints

--- commander_api/panther_waypoint_following.py
+++ commander_api/panther_waypoint_following.py
@@ -29,8 +29,10 @@ def main():
     rclpy.init()        # This is where the communication is initialized.
     nav = BasicNavigator(namespace='panther')
 
+    # filepath = os.path.join(get_package_share_directory('commander_api'), 'config', 'panther_waypoints.txt')
+
     filepath = os.path.join(
-        get_package_share_directory('commander_api'), 'config', 'panther_waypoints.txt')
+        get_package_share_directory('commander_api'), 'config', 'real_panther_lab_waypoints.txt')
 
     # --- Set initial pose
     initial_pose = create_pose_stamped(nav, 0.0, 0.0, 0.0)
